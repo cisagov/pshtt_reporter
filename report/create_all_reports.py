f8782f06208d07c380ec0bfe305dd5e604511e97
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-01-17 16:16:27 -0500

    Made report.sh similar to the identically-named file in trustymail_reporter

--- report/create_all_reports.py
+++ report/create_all_reports.py
@@ -3,13 +3,14 @@
 import os
 import csv
 
-SHARED_DATA_DIR = '/home/shared/'
+HOME_DIR = '/home/reporter'
+SHARED_DATA_DIR = HOME_DIR + '/shared/'
 
 def main():
     agency_csv = open(SHARED_DATA_DIR + "artifacts/unique-agencies.csv")
 
     for row in sorted(csv.reader(agency_csv)):
-        bashCommand = "/home/scanner/report/generate_https_scan_report.py " + '"' + row[0] + '"'
+        bashCommand = HOME_DIR + "/report/generate_https_scan_report.py " + '"' + row[0] + '"'
         os.system(bashCommand)
 
 if __name__ == "__main__":
