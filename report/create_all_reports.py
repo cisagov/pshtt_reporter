f446c00b17eb80bd0e229f346dde8ebb5b5a1844
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2019-01-15 11:24:25 -0500

    Use a with clause to ensure file handle is cleaned up

--- report/create_all_reports.py
+++ report/create_all_reports.py
@@ -6,12 +6,13 @@ import csv
 HOME_DIR = '/home/reporter'
 SHARED_DATA_DIR = HOME_DIR + '/shared/'
 
-def main():
-    agency_csv = open(SHARED_DATA_DIR + "artifacts/unique-agencies.csv")
 
-    for row in sorted(csv.reader(agency_csv)):
-        bashCommand = HOME_DIR + "/report/generate_https_scan_report.py " + '"' + row[0] + '"'
-        os.system(bashCommand)
+def main():
+    with open(SHARED_DATA_DIR + "artifacts/unique-agencies.csv") as agency_csv:
+        for row in sorted(csv.reader(agency_csv)):
+            bashCommand = HOME_DIR + "/report/generate_https_scan_report.py " + '"' + row[0] + '"'
+            os.system(bashCommand)
+
 
 if __name__ == "__main__":
     main()
