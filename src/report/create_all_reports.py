e033892b742b3c433d217585684baec0af71ce26
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2022-02-04 17:29:49 -0500

    Prefer subprocess to os.system

--- src/report/create_all_reports.py
+++ src/report/create_all_reports.py
@@ -3,7 +3,11 @@
 
 # Standard Python Libraries
 import csv
-import os
+
+# generate_https_scan_report.py isn't written in a way that easily
+# allows it to be run in any other way, but Bandit doesn't like us to
+# use subprocess.  Hence the nosec.
+import subprocess  # nosec B404
 
 HOME_DIR = "/home/cisa"
 SHARED_DATA_DIR = HOME_DIR + "/shared/"
@@ -13,11 +17,12 @@ def main():
     """Generate all reports."""
     with open(SHARED_DATA_DIR + "artifacts/unique-agencies.csv") as agency_csv:
         for row in sorted(csv.reader(agency_csv)):
-            bashCommand = f'{HOME_DIR}/report/generate_https_scan_report.py "row[0]"'
+            bashCommand = [f"{HOME_DIR}/report/generate_https_scan_report.py", row[0]]
             # generate_https_scan_report.py isn't written in a way
-            # that easily allows it to be run in any other way.  Hence
-            # the nosec.
-            os.system(bashCommand)  # nosec B605
+            # that easily allows it to be run in any other way, but
+            # Bandit doesn't like us to use subprocess.  Hence the
+            # nosec.
+            subprocess.run(bashCommand)  # nosec B404
 
 
 if __name__ == "__main__":
